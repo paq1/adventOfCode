a369a8171eabb62668113c9198a4a4484adfdf85
paq1 <p.paquin1016@laposte.net>
2022-11-23 05:03:01 +0100

    feat: lecture dans un fichier OK (todo: mettre ca dans un trait

--- challenge_2015/src/main.rs
+++ challenge_2015/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
+fn read_input(path: &str) -> Vec<String> {
+    let chaines_opt = fs::read_to_string(path)
+        .ok()
+        .map(|chaine| chaine.split("\n").map(|c| c.to_string()).collect::<Vec<String>>());
+
+    match chaines_opt {
+        Some(chaines) => chaines,
+        None => vec![]
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_input("inputs/day1/input1.txt");
+    input
+        .into_iter()
+        .enumerate()
+        .for_each(|inp| println!("[index:{}] {}", inp.0, inp.1));
 }
