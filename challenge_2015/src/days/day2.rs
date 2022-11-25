b9e2e2273bdfaa15d520e335c20960c400e10392
Pierre Paquin <p.paquin1016@laposte.net>
2022-11-25 11:26:36 +0100

    feat(advent_of_code_2015): day2 part2

--- challenge_2015/src/days/day2.rs
+++ challenge_2015/src/days/day2.rs
@@ -42,7 +42,6 @@ impl Day for Day2 {
                     })
                     .collect::<Vec<i32>>();
                 let (l, w, h) = if let [l, w, h] = dimensions[..] { (l, w, h) } else { (-1, -1, -1) };
-                println!("{l},{w},{h}");
                 let mut sort = dimensions.clone();
                 sort.sort();
                 2*l*w + 2*w*h + 2*l*h + sort[0]*sort[1]
@@ -53,6 +52,39 @@ impl Day for Day2 {
     }
 
     fn response_2(&self) -> String {
-        "".to_string()
+        let res = self
+            .input1()
+            .iter()
+            .enumerate()
+            .map(|(ligne, chaine)| {
+                let dimensions = chaine
+                    .split("x")
+                    .enumerate()
+                    .map(|(index, element)| {
+                        let transformed_element = element.to_string();
+
+                        let retrans = if index == 2 && ligne != self.input1().len()-1 {
+                            transformed_element[0..transformed_element.len()-1].to_string()
+                        } else {
+                            transformed_element
+                        };
+
+                        let val = match retrans.parse::<i32>() {
+                            Ok(value) => value,
+                            Err(_) => {
+                                panic!("conversion impossible de {}", element);
+                            }
+                        };
+                        val
+                    })
+                    .collect::<Vec<i32>>();
+                let (l, w, h) = if let [l, w, h] = dimensions[..] { (l, w, h) } else { (-1, -1, -1) };
+                let mut sort = dimensions.clone();
+                sort.sort();
+                2 * sort[0] + 2 * sort[1] + l * w * h
+            })
+            .sum::<i32>();
+
+        format!("{}", res)
     }
 }
