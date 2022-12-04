a14c88cbc56c6059fdc7b26c271aaaf20aed6818
Pierre Paquin <p.paquin1016@laposte.net>
2022-12-04 11:07:57 +0100

    feat(challenge_2022): day 2 part 2 OK 😎

--- challenge_2022/src/days/day2.rs
+++ challenge_2022/src/days/day2.rs
@@ -16,6 +16,37 @@ impl Day2 {
             _   => None
         }
     }
+
+    fn win(mouv: Option<PlayEnum>) -> Option<PlayEnum> {
+        match mouv {
+            Some(PlayEnum::Rock) => Some(PlayEnum::Paper),
+            Some(PlayEnum::Paper) => Some(PlayEnum::Scissors),
+            Some(PlayEnum::Scissors) => Some(PlayEnum::Rock),
+            _ => None
+        }
+    }
+
+    fn draw(mouv: Option<PlayEnum>) -> Option<PlayEnum> {
+        mouv
+    }
+
+    fn lose(mouv: Option<PlayEnum>) -> Option<PlayEnum> {
+        match mouv {
+            Some(PlayEnum::Rock) => Some(PlayEnum::Scissors),
+            Some(PlayEnum::Paper) => Some(PlayEnum::Rock),
+            Some(PlayEnum::Scissors) => Some(PlayEnum::Paper),
+            _ => None
+        }
+    }
+
+    fn kind_of_fn(mouv: Option<PlayEnum>) -> impl Fn(Option<PlayEnum>) -> Option<PlayEnum> {
+        match mouv {
+            Some(PlayEnum::Rock) => Day2::lose,
+            Some(PlayEnum::Paper) => Day2::draw,
+            Some(PlayEnum::Scissors) => Day2::win,
+            _ => Day2::lose
+        }
+    }
 }
 
 impl Day for Day2 {
@@ -41,7 +72,6 @@ impl Day for Day2 {
             .into_iter()
             .map(|movement: (Option<PlayEnum>, Option<PlayEnum>)| {
                 let mvt = (movement.0.unwrap(), movement.1.unwrap());
-                println!("{:?}", mvt);
                 let bonus_score = (mvt.0.get_value(), mvt.1.get_value());
                 match mvt {
                     (a, b) if a == b => (3 + bonus_score.0, 3 + bonus_score.1),
@@ -59,17 +89,50 @@ impl Day for Day2 {
             })
             .fold((0, 0), |acc, current| (acc.0 + current.0, acc.1 + current.1));
 
-        println!("scores {:?}", scores);
-
         scores.1.to_string()
     }
 
     fn response_2(&self) -> String {
-        "???".to_string()
+        let scores = self.clean_input()
+            .iter()
+            .map(|current_line: &String| {
+                current_line
+                    .split(" ")
+                    .map(|chaine: &str| chaine.chars().nth(0).unwrap())
+                    .map(|c| self.mapping_element(c))
+                    .fold((None, None), |acc, current| {
+                        if acc.0.is_none() {
+                            (current, None)
+                        } else {
+                            (acc.0.clone(), Day2::kind_of_fn(current)(acc.0))
+                        }
+                    })
+            })
+            .into_iter()
+            .map(|movement: (Option<PlayEnum>, Option<PlayEnum>)| {
+                let mvt = (movement.0.unwrap(), movement.1.unwrap());
+                let bonus_score = (mvt.0.get_value(), mvt.1.get_value());
+                match mvt {
+                    (a, b) if a == b => (3 + bonus_score.0, 3 + bonus_score.1),
+                    (PlayEnum::Rock, PlayEnum::Scissors) => (6 + bonus_score.0, 0 + bonus_score.1),
+                    (PlayEnum::Scissors, PlayEnum::Rock) => (0 + bonus_score.0, 6 + bonus_score.1),
+                    (PlayEnum::Rock, PlayEnum::Paper) => (0 + bonus_score.0, 6 + bonus_score.1),
+                    (PlayEnum::Paper, PlayEnum::Rock) => (6 + bonus_score.0, 0 + bonus_score.1),
+                    (PlayEnum::Paper, PlayEnum::Scissors) => (0 + bonus_score.0, 6 + bonus_score.1),
+                    (PlayEnum::Scissors, PlayEnum::Paper) => (6 + bonus_score.0, 0 + bonus_score.1),
+                    _ => {
+                        println!("pb mapping");
+                        (0, 0)
+                    }
+                }
+            })
+            .fold((0, 0), |acc, current| (acc.0 + current.0, acc.1 + current.1));
+
+        scores.1.to_string()
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum PlayEnum {
     Rock,
     Paper,
